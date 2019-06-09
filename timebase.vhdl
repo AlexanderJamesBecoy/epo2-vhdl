7ea2aaa65e23d1f9bd8914169b6f7a6914f3a4b8
Periodic1911 <dstijns@gmail.com>
2019-06-09 20:17:27 +0000

    added generic to timebase

--- timebase.vhdl
+++ timebase.vhdl
@@ -4,15 +4,16 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity timebase is
+	generic(width: integer);
 	port (	clk		: in	std_logic;
 		reset		: in	std_logic;
 
-		count_out	: out	std_logic_vector (19 downto 0)
+		count_out	: out	std_logic_vector (width-1 downto 0)
 	);
 end entity timebase;
 
 architecture behavioural of timebase is
-signal treg : unsigned(19 downto 0);
+signal treg : unsigned(width-1 downto 0);
 begin
 	process
 	begin
@@ -21,9 +22,6 @@ begin
 		treg <= (others => '0');
 	else
 		treg <= treg + 1; -- counting up at 50 MHz
-		if(treg = "11111111111111111111111111111111111") then
-			treg <= (others => '0'); -- overflow
-		end if;
 	end if;
 	end process;
 	count_out <= std_logic_vector(treg);
