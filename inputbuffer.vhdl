aba2968b24f3fe279431c79d4cdf811820df2120
Periodic1911 <dstijns@gmail.com>
2019-06-09 22:00:52 +0000

    added singe buffer

--- inputbuffer.vhdl
+++ inputbuffer.vhdl
@@ -28,3 +28,33 @@ begin
 	
 	y <= ff2;	
 end behaviour;
+
+library IEEE;
+-- Hier komen de gebruikte libraries:
+use ieee.std_logic_1164.all;
+
+entity inputbuffer1 is
+	port (	clk		: in	std_logic;
+		reset		: in	std_logic;
+		x		: in	std_logic;
+		y		: out	std_logic
+	);
+end entity inputbuffer1;
+
+architecture behaviour of inputbuffer1 is
+signal ff1, ff2 : std_logic;
+begin
+	process
+	begin
+		wait until rising_edge(clk);
+		if(reset = '1') then
+			ff1 <= '0';
+			ff2 <= '0';
+		else
+			ff1 <= x;
+			ff2 <= ff1;
+		end if;
+	end process;
+	
+	y <= ff2;	
+end behaviour;
