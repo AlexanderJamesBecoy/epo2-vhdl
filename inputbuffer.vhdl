ae37a9b9c42c1db3cb5285e4ad2ce4dbad478eb3
Periodic1911 <dstijns@gmail.com>
2019-06-09 20:22:55 +0000

    added generic to inputbuffer

--- inputbuffer.vhdl
+++ inputbuffer.vhdl
@@ -3,37 +3,28 @@ library IEEE;
 use ieee.std_logic_1164.all;
 
 entity inputbuffer is
+	generic(width: integer);
 	port (	clk		: in	std_logic;
 		reset		: in	std_logic;
-
-		sensor_l_in	: in	std_logic;
-		sensor_m_in	: in	std_logic;
-		sensor_r_in	: in	std_logic;
-
-		sensor_l_out	: out	std_logic;
-		sensor_m_out	: out	std_logic;
-		sensor_r_out	: out	std_logic
-		
+		x		: in	std_logic_vector(width-1 downto 0);
+		y		: out	std_logic_vector(width-1 downto 0)
 	);
 end entity inputbuffer;
 
 architecture behaviour of inputbuffer is
-signal ff1, ff2 : std_logic_vector(2 downto 0);
+signal ff1, ff2 : std_logic_vector(width-1 downto 0);
 begin
 	process
 	begin
 		wait until rising_edge(clk);
 		if(reset = '1') then
-			ff1 <= "000";
-			ff2 <= "000";
+			ff1 <= (others => '0');
+			ff2 <= (others => '0');
 		else
-			ff1 <= (0 => sensor_l_in, 1 => sensor_m_in, 2 => sensor_r_in);
+			ff1 <= x;
 			ff2 <= ff1;
 		end if;
 	end process;
 	
-	sensor_r_out <= ff2(0);
-	sensor_m_out <= ff2(1);
-	sensor_l_out <= ff2(2);
-	
+	y <= ff2;	
 end behaviour;
