0e52334dd4038a4e47ee54b82c7050533432d352
Daniel Stijns <dstijns@gmail.com>
2019-06-13 06:31:59 +0000

    new toplevel

--- top_tb.vhdl
+++ top_tb.vhdl
@@ -15,275 +15,186 @@ signal an : std_logic_vector(3 downto 0);
 signal sseg : std_logic_vector(7 downto 0);
 begin
 	uut: entity work.top
+		generic map(500)
 		port map(clk,reset,sensor,lmotor,rmotor,tx,rx,mine,sw,btn,led,an,sseg);
 	clk_tick: process
 	begin
 		clk <= '0';
-		wait for 10 ns;
+		wait for 1000 ns;
 		clk <= '1';
-		wait for 10 ns;
+		wait for 1000 ns;
 	end process;
 
 	stim: process
 	begin
 		reset <= '1';
-		wait for 100 ns;
+		wait for 100 us;
 		reset <= '0';
-		sensor <= "010";
-
-		--L=01001100
-		rx <= '0'; --start bit
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '0';
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '0';
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '1';
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '1';
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '0';
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '0';
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '1';
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '0';
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '1'; --stop bit
-		wait for 104 us; -- 1/9600 seconds
-
-		wait for 500 ms;
-
-		sensor <= "000"; -- crossing
-
-		wait for 400 ms;
-
-		sensor <= "111"; -- turn left
-		wait for 100 ms;
-		sensor <= "010";
-
-		--S=01010011
-		rx <= '0'; --start bit
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '1';
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '1';
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '0';
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '0';
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '1';
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '0';
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '1';
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '0';
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '1'; --stop bit
-		wait for 104 us; -- 1/9600 seconds
-
-		wait for 500 ms;
-
-		sensor <= "000"; -- crossing
-
-		wait for 400 ms;
-
-		sensor <= "010";
-
-		--L=01001100
-		rx <= '0'; --start bit
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '0';
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '0';
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '1';
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '1';
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '0';
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '0';
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '1';
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '0';
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '1'; --stop bit
-		wait for 104 us; -- 1/9600 seconds
-
-		wait for 500 ms;
-
-		sensor <= "000"; -- crossing
-
-		wait for 400 ms;
-
-		sensor <= "111"; -- turn left
-		wait for 100 ms;
 		sensor <= "010";
 
 		--U=01010101
 		rx <= '0'; --start bit
-		wait for 104 us; -- 1/9600 seconds
+		wait for 10.4 ms; -- 1/96 seconds
 		rx <= '1';
-		wait for 104 us; -- 1/9600 seconds
+		wait for 10.4 ms; -- 1/96 seconds
 		rx <= '0';
-		wait for 104 us; -- 1/9600 seconds
+		wait for 10.4 ms; -- 1/96 seconds
 		rx <= '1';
-		wait for 104 us; -- 1/9600 seconds
+		wait for 10.4 ms; -- 1/96 seconds
 		rx <= '0';
-		wait for 104 us; -- 1/9600 seconds
+		wait for 10.4 ms; -- 1/96 seconds
 		rx <= '1';
-		wait for 104 us; -- 1/9600 seconds
+		wait for 10.4 ms; -- 1/96 seconds
 		rx <= '0';
-		wait for 104 us; -- 1/9600 seconds
+		wait for 10.4 ms; -- 1/96 seconds
 		rx <= '1';
-		wait for 104 us; -- 1/9600 seconds
+		wait for 10.4 ms; -- 1/96 seconds
 		rx <= '0';
-		wait for 104 us; -- 1/9600 seconds
+		wait for 10.4 ms; -- 1/96 seconds
 		rx <= '1'; --stop bit
-		wait for 104 us; -- 1/9600 seconds
-
-		wait for 500 ms;
-
-		sensor <= "111"; -- Edge
-
-		wait for 200 ms;
-
-		sensor <= "010";
-
-		--S=01010011
-		rx <= '0'; --start bit
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '1';
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '1';
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '0';
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '0';
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '1';
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '0';
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '1';
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '0';
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '1'; --stop bit
-		wait for 104 us; -- 1/9600 seconds
+		wait for 10.4 ms; -- 1/96 seconds
 
 		wait for 500 ms;
 
 		sensor <= "000"; -- crossing
 
-		wait for 400 ms;
-
-		sensor <= "010";
-
-		--R=01010010
-		rx <= '0'; --start bit
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '0';
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '1';
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '0';
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '0';
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '1';
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '0';
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '1';
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '0';
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '1'; --stop bit
-		wait for 104 us; -- 1/9600 seconds
-
-		wait for 500 ms;
-
-		sensor <= "000"; -- crossing
-
-		wait for 400 ms;
-
-		sensor <= "111"; -- turn right
 		wait for 100 ms;
+
 		sensor <= "010";
+
+		--U=01010101
+		rx <= '0'; --start bit
+		wait for 10.4 ms; -- 1/96 seconds
+		rx <= '1';
+		wait for 10.4 ms; -- 1/96 seconds
+		rx <= '0';
+		wait for 10.4 ms; -- 1/96 seconds
+		rx <= '1';
+		wait for 10.4 ms; -- 1/96 seconds
+		rx <= '0';
+		wait for 10.4 ms; -- 1/96 seconds
+		rx <= '1';
+		wait for 10.4 ms; -- 1/96 seconds
+		rx <= '0';
+		wait for 10.4 ms; -- 1/96 seconds
+		rx <= '1';
+		wait for 10.4 ms; -- 1/96 seconds
+		rx <= '0';
+		wait for 10.4 ms; -- 1/96 seconds
+		rx <= '1'; --stop bit
+		wait for 10.4 ms; -- 1/96 seconds
+
+		wait for 500 ms;
+
+		sensor <= "000"; -- crossing
+
+		wait for 100 ms;
+
+		sensor <= "010";
+
+		--U=01010101
+		rx <= '0'; --start bit
+		wait for 10.4 ms; -- 1/96 seconds
+		rx <= '1';
+		wait for 10.4 ms; -- 1/96 seconds
+		rx <= '0';
+		wait for 10.4 ms; -- 1/96 seconds
+		rx <= '1';
+		wait for 10.4 ms; -- 1/96 seconds
+		rx <= '0';
+		wait for 10.4 ms; -- 1/96 seconds
+		rx <= '1';
+		wait for 10.4 ms; -- 1/96 seconds
+		rx <= '0';
+		wait for 10.4 ms; -- 1/96 seconds
+		rx <= '1';
+		wait for 10.4 ms; -- 1/96 seconds
+		rx <= '0';
+		wait for 10.4 ms; -- 1/96 seconds
+		rx <= '1'; --stop bit
+		wait for 10.4 ms; -- 1/96 seconds
+
+		wait for 500 ms;
+
+		sensor <= "000"; -- crossing
+
+		wait for 100 ms;
+
+		sensor <= "010";
+
+		wait for 100 ms;
 
 		mine <= '1'; -- Mine detected
 		wait for 1.9 ms;
 		mine <= '0';
 
-		wait for 400 ms;
+		wait for 100 ms;
 
 
-		--R=01010010
+		--U=01010101
 		rx <= '0'; --start bit
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '0';
-		wait for 104 us; -- 1/9600 seconds
+		wait for 10.4 ms; -- 1/96 seconds
 		rx <= '1';
-		wait for 104 us; -- 1/9600 seconds
+		wait for 10.4 ms; -- 1/96 seconds
 		rx <= '0';
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '0';
-		wait for 104 us; -- 1/9600 seconds
+		wait for 10.4 ms; -- 1/96 seconds
 		rx <= '1';
-		wait for 104 us; -- 1/9600 seconds
+		wait for 10.4 ms; -- 1/96 seconds
 		rx <= '0';
-		wait for 104 us; -- 1/9600 seconds
+		wait for 10.4 ms; -- 1/96 seconds
 		rx <= '1';
-		wait for 104 us; -- 1/9600 seconds
+		wait for 10.4 ms; -- 1/96 seconds
 		rx <= '0';
-		wait for 104 us; -- 1/9600 seconds
+		wait for 10.4 ms; -- 1/96 seconds
+		rx <= '1';
+		wait for 10.4 ms; -- 1/96 seconds
+		rx <= '0';
+		wait for 10.4 ms; -- 1/96 seconds
 		rx <= '1'; --stop bit
-		wait for 104 us; -- 1/9600 seconds
+		wait for 10.4 ms; -- 1/96 seconds
 
 		wait for 500 ms;
 
 		sensor <= "000"; -- crossing
+
+		wait for 100 ms;
+
+		sensor <= "010";
+
+		--R=01010010
+		rx <= '0'; --start bit
+		wait for 10.4 ms; -- 1/96 seconds
+		rx <= '0';
+		wait for 10.4 ms; -- 1/96 seconds
+		rx <= '1';
+		wait for 10.4 ms; -- 1/96 seconds
+		rx <= '0';
+		wait for 10.4 ms; -- 1/96 seconds
+		rx <= '0';
+		wait for 10.4 ms; -- 1/96 seconds
+		rx <= '1';
+		wait for 10.4 ms; -- 1/96 seconds
+		rx <= '0';
+		wait for 10.4 ms; -- 1/96 seconds
+		rx <= '1';
+		wait for 10.4 ms; -- 1/96 seconds
+		rx <= '0';
+		wait for 10.4 ms; -- 1/96 seconds
+		rx <= '1'; --stop bit
+		wait for 10.4 ms; -- 1/96 seconds
+
+		wait for 500 ms;
+
+		sensor <= "000"; -- crossing
+
+		wait for 100 ms;
+
+		sensor <= "010"; -- line
 
 		wait for 400 ms;
 
 		sensor <= "111"; -- turn right
 		wait for 100 ms;
-		sensor <= "010";
-
-		--U=01010101
-		rx <= '0'; --start bit
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '1';
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '0';
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '1';
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '0';
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '1';
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '0';
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '1';
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '0';
-		wait for 104 us; -- 1/9600 seconds
-		rx <= '1'; --stop bit
-		wait for 104 us; -- 1/9600 seconds
-
-		wait for 500 ms;
-
-		sensor <= "000"; -- crossing
-
-		wait for 200 ms;
-
 		sensor <= "010";
 
 		wait;
